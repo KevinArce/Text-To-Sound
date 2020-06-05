aa6235dc0534e97b0d72eefb9a7c65fd10df35f8
Arce <arcetechnologies@gmail.com>
2020-06-05 16:18:11 -0600

    Text To Sound v0.1

---
+++ Text To Sound.py
@@ -0,0 +1,23 @@
+#Text To Sound
+from tika import parser
+from gtts import gTTS, gTTSError
+from playsound import playsound, PlaysoundException
+
+#FUTURES IMPORTS 
+#for IA and Text Recognition Pattern
+#from texts import text_counter, text_training
+#from sklearn.feature_extraction.text 
+
+#Giving a name to the file
+filename = input ("Write a name to your File: ")
+
+#Write a path to look the file up
+file = input ("Enter the path of your File: ")
+
+parser = parser.from_file(file)
+txt = parser['content']
+speech = gTTS(text = txt, lang = 'en')
+speech.save(filename)
+
+print ('Good to Go')
+print ('Now change the extension of the resulting file to ".MP3/.ACC/.WAV"')
